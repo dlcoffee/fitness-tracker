9facf06367e27d3ea54580932b9dbf35f7993195
daniel tea <daniel.l.tea@gmail.com>
2024-03-03 11:26:40 -0800

    add custom cell render

--- src/app/columns.tsx
+++ src/app/columns.tsx
@@ -1,6 +1,9 @@
 'use client'
 
+import { useState, useEffect } from 'react'
 import { ColumnDef } from '@tanstack/react-table'
+
+import { Input } from '@/components/ui/input'
 
 //import type { Workout } from '@/db/schema'
 
@@ -21,9 +24,63 @@ export const columns: ColumnDef<WorkoutLog>[] = [
   {
     accessorKey: 'weight',
     header: 'Weight',
+    cell: function Cell({ getValue, row }) {
+      const initialValue = getValue() || ''
+      const [value, setValue] = useState(initialValue)
+
+      const handleOnBlur = () => {
+        const { id, ...data } = { ...row.original }
+
+        if (value && data.weight !== value) {
+          data.weight = parseInt(value as string)
+          console.log({ id, data })
+        }
+      }
+
+      // If the initialValue is changed external, sync it up with our state
+      useEffect(() => {
+        setValue(initialValue)
+      }, [initialValue])
+
+      return (
+        <Input
+          value={value as string}
+          onBlur={handleOnBlur}
+          onChange={(e) => setValue(e.target.value)}
+          type="number"
+        ></Input>
+      )
+    },
   },
   {
     accessorKey: 'repititions',
     header: 'Repititions',
+    cell: function Cell({ getValue, row }) {
+      const initialValue = getValue() || ''
+      const [value, setValue] = useState(initialValue)
+
+      const handleOnBlur = () => {
+        const { id, ...data } = { ...row.original }
+
+        if (value && data.repititions !== value) {
+          data.repititions = parseInt(value as string)
+          console.log({ id, data })
+        }
+      }
+
+      // If the initialValue is changed external, sync it up with our state
+      useEffect(() => {
+        setValue(initialValue)
+      }, [initialValue])
+
+      return (
+        <Input
+          value={value as string}
+          onBlur={handleOnBlur}
+          onChange={(e) => setValue(e.target.value)}
+          type="number"
+        ></Input>
+      )
+    },
   },
 ]
